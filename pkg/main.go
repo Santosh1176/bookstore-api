a90b06c9a36448f7ae630f8322ae960bde74906a
Santosh Kaluskar <dtshbl@gmail.com>
2023-05-13 16:32:28 +0530

    CommitSHA with new Import

--- pkg/main.go
+++ pkg/main.go
@@ -9,6 +9,9 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/goconvey/convey"
+
 	_ "github.com/lib/pq"
 )
 
@@ -61,23 +64,18 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	commitSHA, err := getCommitSHA()
-	if err != nil {
-		http.Error(w, "Error retrieving commit SHA", http.StatusInternalServerError)
-		return
-	}
+	convey.Convey("When the index page is requested", func() {
+		commitSHA, err := getCommitSHA()
+		convey.So(err, should.BeNil)
 
-	data := struct {
-		CommitSHA string
-	}{
-		CommitSHA: commitSHA,
-	}
-	tpl.ExecuteTemplate(w, "index.gohtml", data)
+		data := struct {
+			CommitSHA string
+		}{
+			CommitSHA: commitSHA,
+		}
+		convey.So(tpl.ExecuteTemplate(w, "index.gohtml", data), should.BeNil)
+	})
 
-	err = tpl.Execute(w, data)
-	if err != nil {
-		return
-	}
 	http.Redirect(w, r, "/books", http.StatusSeeOther)
 
 }
